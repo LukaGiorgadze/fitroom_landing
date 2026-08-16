44bb87825bd74c75ee8609a7caed44031fd93185
Luka Giorgadze <luka.giorgadze@gmail.com>
2026-08-16 15:40:58 +0400

    add ms clarity

--- public/analytics.js
+++ public/analytics.js
@@ -1,5 +1,6 @@
-(function initializeGoogleAnalytics() {
-  const measurementId = "G-WN60T53PZM";
+(function initializeAnalytics() {
+  const googleMeasurementId = "G-WN60T53PZM";
+  const clarityProjectId = "y394crqsgh";
 
   window.dataLayer = window.dataLayer || [];
   window.gtag =
@@ -9,10 +10,22 @@
     };
 
   window.gtag("js", new Date());
-  window.gtag("config", measurementId);
+  window.gtag("config", googleMeasurementId);
 
   const googleTag = document.createElement("script");
   googleTag.async = true;
-  googleTag.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
+  googleTag.src = `https://www.googletagmanager.com/gtag/js?id=${googleMeasurementId}`;
   document.head.append(googleTag);
+
+  window.clarity =
+    window.clarity ||
+    function clarity() {
+      window.clarity.q = window.clarity.q || [];
+      window.clarity.q.push(arguments);
+    };
+
+  const clarityTag = document.createElement("script");
+  clarityTag.async = true;
+  clarityTag.src = `https://www.clarity.ms/tag/${clarityProjectId}`;
+  document.head.append(clarityTag);
 })();
